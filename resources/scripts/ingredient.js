a5351d57f3b781f8d99452f098bb84836e6fb37a
AnimatedOutcast <144029652+AnimatedOutcast@users.noreply.github.com>
2024-05-05 14:55:22 -0400

    ingredient.html tweaked

--- resources/scripts/ingredient.js
+++ resources/scripts/ingredient.js
@@ -25,19 +25,42 @@ if (ingredient.servingSize > 1) {
     document.body.querySelector('#servingSize').textContent = ingredient.servingSize + " " + ingredient.servingUnit;
 }
 document.body.querySelector("#caloriesPerServing").textContent = ingredient.caloriesPerServing;
+//cup, Tbsp, tsp:
+let netCups;
+let netTbsp;
+let netTsp;
+switch (ingredient.servingUnit) {
+    case "cup":
+        netCups = ingredient.totalServings * ingredient.servingSize;
+        netTbsp = netCups * 16;
+        netTsp = netCups * 48;
+        console.log(`${netCups} cups, ${netTbsp} tablespoons, ${netTsp} teaspoons.`)
+        break;
+    default:
+        break;
+}
 //cost calculator:
 function calculateCost() {
     let costPerUnit;
+    const amount = document.body.querySelector('#amount').value;
     switch (document.body.querySelector('#unitSelect').value) {
         case "ounce":
-            costPerUnit = (ingredient.ingCost / ingredient.netWeight) * document.body.querySelector('#amount').value;
+            costPerUnit = (ingredient.ingCost / ingredient.netWeight) * amount;
             break;
         case "gram":
-            costPerUnit = ((ingredient.ingCost / (ingredient.netWeight * 28.34952)) * document.body.querySelector('#amount').value);
+            costPerUnit = (ingredient.ingCost / (ingredient.netWeight * 28.34952)) * amount;
+            break;
+        case "cup":
+            costPerUnit = (ingredient.ingCost / netCups) * amount;
+            break;
+        case "tbsp":
+            break;
+        case "tsp":
             break;
         default:
             break;
     }
-    alert(`${document.body.querySelector('#amount').value} ${document.body.querySelector('#unitSelect').value}(s) of ${ingredient.ingName} costs ${costPerUnit.toFixed(2)}`);
+    let msg = `${document.body.querySelector('#amount').value} ${document.body.querySelector('#unitSelect').value}(s) of ${ingredient.ingName} costs ${costPerUnit.toFixed(2)}`;
+    document.body.querySelector('#result').textContent = msg;
 }
 //button:
